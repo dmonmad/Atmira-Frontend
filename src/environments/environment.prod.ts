90f236258f1eed239ae504139ac6abc8d346819f
imnightm4re <david.bechkam@hotmail.com>
2021-09-23 21:36:03 +0200

    delete everything from branch

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true
-};
-
-export const urls = {
-  nasaBaseUrl: "https://api.nasa.gov",
-  planetaryApi: "/planetary/apod?api_key=zdUP8ElJv1cehFM0rsZVSQN7uBVxlDnu4diHlLSb&date=SAMPLE_DATE"
-}
