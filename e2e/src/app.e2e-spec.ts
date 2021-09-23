625b1ad79f011bad099c0d6fca28881d251c0fdf
imnightm4re <david.bechkam@hotmail.com>
2021-09-23 21:46:54 +0200

    last update

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('nasa-front app is running!');
+    expect(await page.getTitleText()).toEqual('atmira-frontend app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/nasa-front'),
+      dir: require('path').join(__dirname, './coverage/atmira-frontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
